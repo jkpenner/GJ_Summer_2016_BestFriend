bbefcb492965dd87c03d24eddd18febc514eb14f
Jacob Penner <jkpenner@live.com>
2016-06-16 14:04:27 -0700

    Add Object Pooling Utility Scripts

---
+++ Assets/Scripts/Utilities/Pooling/Interfaces/IPoolableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPoolableObject {
+    int Id { get; set; }
+    string Name { get; set; }
+    GameObject Prefab { get; set; }
+
+	bool PoolRestricted { get; set; }
+	int PoolCount { get; set; }
+}
---
+++ Assets/Scripts/Utilities/Pooling/Pools/SpawnPool.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SpawnPool : AbstractPool<SpawnPool> {
+    public override void InitializePool() {
+        for (int i = 0; i < SpawnDatabase.Instance.Count; i++) {
+            Debug.Log("Adding Object to Spawn Pool");
+            AddAssetToPool(SpawnDatabase.Instance.Get(i) as IPoolableObject);
+        }
+    }
+
+    public override IPoolableObject GetObject(int id) {
+        return SpawnDatabase.GetAsset(id) as IPoolableObject;
+    }
+
+    public override bool IsObjectRestricted(int id) {
+        var asset = SpawnDatabase.GetAsset(id) as IPoolableObject;
+        if (asset != null) {
+            return asset.PoolRestricted;
+        }
+        return false;
+    }
+
+    public GameObject ActivateObject(GameObject prefab) {
+        return ActivateObject(prefab.name);
+    }
+
+    public GameObject ActivateObject(string name) {
+        return Activate(GetObjectIdFromName(name));
+    }
+
+    static public GameObject Activate(GameObject prefab) {
+        return Instance.ActivateObject(prefab.name);
+    }
+
+    static public GameObject Activate(string name) {
+        return Instance.ActivateObject(name);
+    }
+
+    static public GameObject Activate(int id) {
+        return Instance.ActivateObject(id);
+    }
+
+    static public void Deactivate(GameObject gameObject) {
+        Instance.DeactivateObject(gameObject);
+    }
+}
