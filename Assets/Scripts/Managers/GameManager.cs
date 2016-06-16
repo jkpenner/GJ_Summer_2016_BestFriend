c26c2e14e7b3dff7e46c713eb00828400356c0bf
Jacob Penner <jkpenner@live.com>
2016-06-15 21:55:50 -0700

    Add Initial GameManager Script and Prefab

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Singleton<GameManager> {
+    public enum State { None, Active, Pause, Win, GameOver }
+
+    public enum EventType { StateEnter, StateExit }
+
+    public delegate void GameStateEvent(State state);
+    private GameStateEvent OnGameStateEnter;
+    private GameStateEvent OnGameStateExit;
+
+    private State _activeState = State.None;
+
+    private void Awake() {
+        if (Instance == this) {
+            DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+
+    static public State ActiveState {
+        get { return Instance._activeState; }
+        set { SetState(value); } 
+    }
+
+    static public void SetState(State newState) {
+        if (Instance._activeState != newState) {
+            Debug.LogFormat("[{0}]: State assigned to {1}", 
+                "GameManager", ActiveState.ToString());
+
+            // Call State Exit for previous ActiveState
+            if (Instance.OnGameStateExit != null) {
+                Instance.OnGameStateExit(ActiveState);
+            }
+
+            Instance._activeState = newState;
+
+            // Call State Enter for new ActiveState
+            if (Instance.OnGameStateEnter != null) {
+                Instance.OnGameStateEnter(ActiveState);
+            }
+        }
+    }
+
+    static public void AddListener(EventType type, GameStateEvent func) {
+        switch (type) {
+            case EventType.StateEnter: Instance.OnGameStateEnter += func; break;
+            case EventType.StateExit: Instance.OnGameStateExit += func; break;
+        }
+    }
+
+    static public void RemoveListener(EventType type, GameStateEvent func) {
+        switch (type) {
+            case EventType.StateEnter: Instance.OnGameStateEnter -= func; break;
+            case EventType.StateExit: Instance.OnGameStateExit -= func; break;
+        }
+    }
+}
