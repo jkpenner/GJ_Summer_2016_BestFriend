f5f07da56c953dbfc30bb077532a770a71713e63
Jacob Penner <jkpenner@live.com>
2016-06-16 14:04:09 -0700

    Add Database Utility Scripts

---
+++ Assets/Scripts/Utilities/Databases/BaseDatabaseAsset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class BaseDatabaseAsset {
+    [SerializeField]
+    private int _id;
+
+    public int Id {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    public virtual string Name {
+        get {
+            return Id.ToString("D4");
+        }
+        set {
+
+        }
+    }
+}
+
