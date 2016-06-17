a13ac0bcdb96b51ee0683ca2fa2fe454f7e187c9
Jacob Penner <jkpenner@live.com>
2016-06-16 21:08:26 -0700

    Add Sound Manager to scenes

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : Singleton<SoundManager> {
+    private void Awake() {
+        if (Instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
