771b7b0ed7d7a99596ddb77b77bcf72db9952a87
Erik Sundberg <erik@sundberg.io>
2016-06-18 15:17:24 -0700

    More chicken and cow tweaks

--- Assets/Scripts/Animals/PlayerController.cs
+++ Assets/Scripts/Animals/PlayerController.cs
@@ -8,6 +8,16 @@ public class PlayerController : MonoBehaviour {
 		DISABLED
 	}
 
+	public enum Animal{
+		CHICKEN,
+		COW,
+		PIG
+	}
+
+	public Animal animal;
+	public float flightForce = 400f; 
+	public float grindSpeed = 15f;
+
 	State _state;
 	PlayerJump playerJump;
 	PlayerMove playerMove;
@@ -20,24 +30,34 @@ public class PlayerController : MonoBehaviour {
 		rigidBody = gameObject.GetComponent<Rigidbody2D>();
 		ChangeState(State.ACTIVE);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-
+		//TODO: move chicken flight to own class
+		if(_state == State.DISABLED && animal == Animal.CHICKEN){
+			rigidBody.AddForce(Vector2.up * flightForce);
+		}
 	}
 
 	//Collision with other animals
 	void OnCollisionEnter2D(Collision2D collision){
-		if(collision.transform.tag == "Anchor" && _state == State.ACTIVE){
+		if(collision.transform.tag == "Anchor"){
+			if(_state == State.ACTIVE){
+				ChangeState(State.DISABLED);
+				ResetPlayer();
+			}
+			foreach(FixedJoint2D fixedJoint in gameObject.GetComponents<FixedJoint2D>()){
+				if(fixedJoint.connectedBody == collision.gameObject) return;
+			}
 			StickObject(collision);
-			ChangeState(State.DISABLED);
-			ResetPlayer();
 		}
 	}
 
 	//Collision with "Grinder"
 	void OnTriggerEnter2D(Collider2D collider){
-		if(_state == State.ACTIVE){
+		if(collider.CompareTag("Destroyer")){
+			Destroy(gameObject);
+		}else if(_state == State.ACTIVE){
 			ResetPlayer();
 		}
 		GrindPlayer();
@@ -60,7 +80,8 @@ public class PlayerController : MonoBehaviour {
 
 	void GrindPlayer(){
 		rigidBody.isKinematic = true;
-		rigidBody.velocity = new Vector2(0,-10f*Time.deltaTime);
+		rigidBody.velocity = Vector2.down * grindSpeed*Time.deltaTime;
+		gameObject.GetComponentInChildren<Animator>().SetBool("isGrinding", true);
 	}
 
 	/*  TODO: Move to own class */
@@ -73,8 +94,8 @@ public class PlayerController : MonoBehaviour {
 		endHinge.frequency = 5f;
 		endHinge.dampingRatio = 1;
 
-		endHinge.anchor = GetVectorOffset(gameObject, collision.gameObject, transform.eulerAngles.z) * 3f;
-		endHinge.connectedAnchor = GetVectorOffset(collision.gameObject, gameObject, collision.transform.eulerAngles.z) * 3f;
+		endHinge.anchor = GetVectorOffset(gameObject, collision.gameObject, transform.eulerAngles.z) * gameObject.GetComponent<CircleCollider2D>().radius;
+		endHinge.connectedAnchor = GetVectorOffset(collision.gameObject, gameObject, collision.transform.eulerAngles.z) * collision.gameObject.GetComponent<CircleCollider2D>().radius;
 	}
 		
 	Vector2 GetVectorOffset(GameObject object1, GameObject object2, float angle){
