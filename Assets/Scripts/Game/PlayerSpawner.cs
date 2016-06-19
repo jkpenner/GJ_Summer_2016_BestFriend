796cc4fdb2e88765ce13d04cd4ad3fceb9bd21ac
Jacob Penner <jkpenner@live.com>
2016-06-18 21:01:29 -0700

    Update PlayerSpawner to use the Character Database

--- Assets/Scripts/Game/PlayerSpawner.cs
+++ Assets/Scripts/Game/PlayerSpawner.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class PlayerSpawner : MonoBehaviour {
-
+    public bool useRandom = true;
 	public GameObject[] playerObjects;
 	public int playerNumber;
+
+    public UICharacterSelector characterSelector;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +19,19 @@ public class PlayerSpawner : MonoBehaviour {
 	}
 
 	public void CreateNewPlayer(){
-		GameObject newPlayer = Instantiate(playerObjects[Random.Range(0, playerObjects.Length)], transform.position, Quaternion.identity) as GameObject;
-		newPlayer.GetComponent<InputMapper>().SetPlayerNumber(playerNumber);
+        if (useRandom || (characterSelector == null && !useRandom)) {
+            // Pull a random character from the character database
+            var asset = CharacterDatabase.Instance.Get(Random.Range(0, CharacterDatabase.Instance.Count));
+            // Instantiate the prefab from the selected asset
+            GameObject newPlayer = Instantiate(asset.Prefab, transform.position, Quaternion.identity) as GameObject;
+            newPlayer.GetComponent<InputMapper>().SetPlayerNumber(playerNumber);
+        } else {
+            // Show the character selector and spawn the selected character
+            characterSelector.Display((id) => {
+                var asset = CharacterDatabase.GetAsset(id);
+                var instance = (GameObject)Instantiate(asset.Prefab, transform.position, Quaternion.identity);
+                instance.GetComponent<InputMapper>().SetPlayerNumber(playerNumber);
+            }, Camera.main.WorldToScreenPoint(transform.position));
+        }
 	}
 }
