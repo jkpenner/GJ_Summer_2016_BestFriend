862cfad11040314d065e5567067a72b75915b77c
Erik Sundberg <erik@sundberg.io>
2016-06-18 20:43:51 -0700

    More tweaks to mechanics
    Level is more like what it will be in the final game

---
+++ Assets/Scripts/Animals/PlayerDive.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDive : MonoBehaviour {
+
+	public float diveForce = 15000f;
+
+	Rigidbody2D rigidBody;
+	string jumpInput;
+	bool canDive = false;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = gameObject.GetComponent<Rigidbody2D>();
+		jumpInput = gameObject.GetComponent<InputMapper>().GetMappedInput("Jump");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Dive();
+	}
+
+	//Collision with other animals
+	void OnCollisionEnter2D(Collision2D collision){
+		canDive = true;
+	}
+
+	void Dive(){
+		if(Input.GetButtonDown(jumpInput) && Mathf.Abs(rigidBody.velocity.y) > 0.1){
+			Debug.Log("dive");
+			rigidBody.AddForce(new Vector2(0f, -diveForce));
+			canDive = false;
+		}
+	}
+}
